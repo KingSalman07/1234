d9397cf7f76ef871f3fb2603744847b28fec9088
KingSalman07 <158686074+KingSalman07@users.noreply.github.com>
2024-02-05 21:44:38 +0700

    create new folder

---
+++ Rafi/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Folder Info Cena</title>
+</head>
+<body>
+    <b>Aku R. Rafi Salman Harashta, S.T. </b>
+</body>
+</html>
