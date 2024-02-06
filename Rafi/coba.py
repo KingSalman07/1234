f26cfe0c6fa88f17f8e8715fad5f20f8e862a197
KingSalman07 <158686074+KingSalman07@users.noreply.github.com>
2024-02-06 22:18:13 +0700

    create new folder

---
+++ Rafi/coba.py
@@ -0,0 +1,7 @@
+number : 7 #int
+number_2 : 97.8 #bilangan float
+boolean_var : True
+
+if __name__ == '__main__':
+    print (number + number_2)
+    print (number_2 + boolean_var)
---
+++ Rafi/main.py
@@ -0,0 +1,7 @@
+number : int =  7 #int
+number_2 : float = 97.8 #bilangan float
+boolean_var =  True
+
+if __name__ == '__main__':
+    print (number + number_2);
+    print (number_2 + boolean_var);
