d8c1fb75c9915795dab31aa345797e647057de27
KingSalman07 <158686074+KingSalman07@users.noreply.github.com>
2024-02-08 21:32:41 +0700

    create new folder

---
+++ Rafi/bagian2.py
@@ -0,0 +1,4 @@
+my_list = [1,2,3,4, 'namaku King Salman', [1,2,4,7]]
+
+if __name__ == '__main__':
+    print (my_list[4])
---
+++ Rafi/revisi.py
@@ -0,0 +1,9 @@
+number :int = 13
+number_2 :float = 98.7
+boolean_var = True #angkanya di tambah 1
+string_var = "Eduwork Tolong Videonya jangan hd minimal FHD, mata saya burem"
+
+if __name__ == '__main__' :
+    print(number + number_2)
+    print(number_2 + boolean_var) 
+    # print(number_2 +  string_var)
